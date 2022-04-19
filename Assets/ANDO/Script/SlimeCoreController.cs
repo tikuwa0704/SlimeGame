d58c0f9dd60a1cddb8910d8655a3a5d7cf0f5b99
tikuwa0704 <60560487+tikuwa0704@users.noreply.github.com>
2022-04-19 16:10:06 +0900

    スライムの移動速度などの調整
    Sceneに反映しました

--- Assets/ANDO/Script/SlimeCoreController.cs
+++ Assets/ANDO/Script/SlimeCoreController.cs
@@ -13,7 +13,6 @@ public class SlimeCoreController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //SlimeMove();
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         inputVertical = Input.GetAxisRaw("Vertical");
         Jump();
@@ -21,43 +20,17 @@ public class SlimeCoreController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        SlimeMove2();
-
+        SlimeMove();
     }
 
     [SerializeField] private Vector3 velocity;         // 移動方向
     [SerializeField] private float moveSpeed = 5.0f;   // 移動速度
 
-    void SlimeMove()
-    {
-        // WASD入力から、XZ平面(水平な地面)を移動する方向(velocity)を得ます
-        velocity = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
-            velocity.z += 1;
-        if (Input.GetKey(KeyCode.A))
-            velocity.x -= 1;
-        if (Input.GetKey(KeyCode.S))
-            velocity.z -= 1;
-        if (Input.GetKey(KeyCode.D))
-            velocity.x += 1;
-
-        // 速度ベクトルの長さを1秒でmoveSpeedだけ進むように調整します
-        velocity = velocity.normalized * moveSpeed * Time.deltaTime;
-
-        // いずれかの方向に移動している場合
-        if (velocity.magnitude > 0)
-        {
-            // プレイヤーの位置(transform.position)の更新
-            // 移動方向ベクトル(velocity)を足し込みます
-            transform.position += velocity;
-        }
-    }
-
     float inputHorizontal;
     float inputVertical;
     Rigidbody rb;
 
-    void SlimeMove2()
+    void SlimeMove()
     {
         // カメラの方向から、X-Z平面の単位ベクトルを取得
         Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
@@ -90,11 +63,12 @@ public class SlimeCoreController : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter(Collision other) //地面に触れた時の処理
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.CompareTag("Ground")) //Groundタグのオブジェクトに触れたとき
+        if (collision.gameObject.CompareTag("Ground")) //Groundタグのオブジェクトに触れたとき
         {
             isGround = true; //isGroundをtrueにする
         }
     }
+    
 }
