8585cc2c4eff5e08127edc6b9db5bc4d8c54ba82
tikuwa0704 <yousuke07041@gmail.com>
2022-04-22 10:57:06 +0900

    凍っていくスクリプト完成
    凍ると消えるようになりました

--- Assets/ANDO/Script/SlimeRender.cs
+++ Assets/ANDO/Script/SlimeRender.cs
@@ -9,8 +9,7 @@ public class SlimeRender : MonoBehaviour
     private readonly Vector4[] _spheres = new Vector4[MaxSphereCount];
     private SphereCollider[] _colliders;
     private Vector4[] _colors = new Vector4[MaxSphereCount];
-    private readonly float[] _spheres_state = new float[MaxSphereCount];
-    private readonly float[] _alpha = new float[MaxSphereCount];
+    
 
     private void Start()
     {
@@ -34,7 +33,9 @@ public class SlimeRender : MonoBehaviour
     {
         // 子のSphereColliderをすべて取得
         _colliders = GetComponentsInChildren<SphereCollider>();
-        
+
+        // シェーダー側の _SphereCount を更新
+        material.SetInt("_SphereCount", _colliders.Length);
 
         // 子のSphereColliderの分だけ、_spheres に中心座標と半径を入れていく
         for (var i = 0; i < _colliders.Length; i++)
@@ -45,26 +46,11 @@ public class SlimeRender : MonoBehaviour
             var radius = t.lossyScale.x * col.radius;
             // 中心座標と半径を格納
             _spheres[i] = new Vector4(center.x, center.y, center.z, radius);
-
-            //_spheres_state[i] = 1;
-            int _state = (int)col.gameObject.GetComponent<SlimeContoroller>().m_state;
-
-            if (_state == 0)
-            {
-                //_colors[i] = new Vector4(1, 1, 1, 1); 
-            }
-            else if (_state == 1)
-            {
-                _colors[i] = new Vector4(0, 0, 1, 0);
-            }
         }
 
         // シェーダー側の _Spheres を更新
         material.SetVectorArray("_Spheres", _spheres);
         material.SetVectorArray("_Colors", _colors);
-        material.SetFloatArray("_SphereState", _spheres_state);
-
-        //Debug.Log(material.GetInt("_SphereState2"));
         
     }
 }
