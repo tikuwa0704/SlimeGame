86d2f1732b9679f48f577264755549e7841b17e0
YoshiduZin <102496757+YoshiduZin@users.noreply.github.com>
2022-05-13 12:20:14 +0900

    ゲームオーバー
    ゲーム画面のプレハブに入っています。

--- Assets/YOSHIZU/Script/GameOver2.cs
+++ Assets/YOSHIZU/Script/GameOver2.cs
@@ -1,26 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class GameOver2 : MonoBehaviour
 {
+    [SerializeField] private GameObject Back_Ground;
+
+    public static int State;
+
+    GameObject object1;
+    GameObject object2;
+    GameObject object3;
+    GameObject object4;
+    GameObject object5;
+    GameObject object6;
+    GameObject object7;
+    GameObject object8;
+
     // Use this for initialization
     void Start()
     {
+        object1 = GameObject.Find("G");
+        object2 = GameObject.Find("A");
+        object3 = GameObject.Find("M");
+        object4 = GameObject.Find("E1");
+        object5 = GameObject.Find("O");
+        object6 = GameObject.Find("V");
+        object7 = GameObject.Find("E2");
+        object8 = GameObject.Find("R");
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-        // ÉLÅ[ì¸óÕ
-        if (Input.GetKeyDown(KeyCode.H))
+    { 
+        if (Back_Ground)
         {
+            var back_Ground = Back_Ground.GetComponent<GameOver1>();
+            if (back_Ground.G_Over)
+            {
+                if (State <= 400)
+                {
+                    State++;
+                }
 
-            transform.DOLocalMoveY(100, 2f).SetEase(Ease.OutBounce);
-            // íµÇÀÇÈÇ¡Ç€Ç≠
-            //transform.DOLocalMoveY(-30f, 2f).SetEase(Ease.OutBounce);
+                if (State == 50)
+                {
+                    object1.transform.DOLocalMoveY(50, 2f).SetEase(Ease.OutBounce);
+                }
+
+                if (State == 100)
+                {
+                    object2.transform.DOLocalMoveY(50, 2f).SetEase(Ease.OutBounce);
+                }
+
+                if (State == 150)
+                {
+                    object3.transform.DOLocalMoveY(50, 2f).SetEase(Ease.OutBounce);
+                }
+                if (State == 200)
+                {
+                    object4.transform.DOLocalMoveY(50, 2f).SetEase(Ease.OutBounce);
+                }
+
+                if (State == 250)
+                {
+                    object5.transform.DOLocalMoveY(50, 2f).SetEase(Ease.OutBounce);
+                }
+
+                if (State == 300)
+                {
+                    object6.transform.DOLocalMoveY(50, 2f).SetEase(Ease.OutBounce);
+                }
+
+                if (State == 350)
+                {
+                    object7.transform.DOLocalMoveY(50, 2f).SetEase(Ease.OutBounce);
+                }
+
+                if (State == 400)
+                {
+                    object8.transform.DOLocalMoveY(50, 2f).SetEase(Ease.OutBounce);
+                }
+            }
         }
     }
 
