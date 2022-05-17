96b95f06ebf28c3733e2008149171ca4f5cff8c6
しょるー <91645801+shorucuro-do@users.noreply.github.com>
2022-05-17 10:33:50 +0900

    スライムシャワー

--- Assets/MORI/scripts/SlimeShower.cs
+++ Assets/MORI/scripts/SlimeShower.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 public class SlimeShower : MonoBehaviour
 {
     public GameObject Slime;
+    public int Heel = 0;//ƒXƒ‰ƒCƒ€ŒÂ”
 
     public void slimeshower()
     {
+        if (SlimeManager.Instance)
+        {
+            SlimeManager.Instance.GenerateSlime(new Vector3(-80.4f, 61.4f, 384.1f));
+        }
+        //GameObject slime = (GameObject)Instantiate(Slime, transform.position, Quaternion.identity);
+    }
 
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Heel <= 99)
+            {
+                slimeshower();
+                Heel++;
+            }
+        }
     }
 }
