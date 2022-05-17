e01f607fb05284e37b0ff58137a70b83f967b9a9
しょるー <91645801+shorucuro-do@users.noreply.github.com>
2022-05-17 10:02:29 +0900

    門部分修正

--- Assets/MORI/scripts/Doorswitch.cs
+++ Assets/MORI/scripts/Doorswitch.cs
@@ -6,12 +6,17 @@ public class Doorswitch : MonoBehaviour
 {
     public GameObject Door;
     public bool on=false;
+    public int a = 0;//スライムの数
     private void OnCollisionEnter(Collision collision)
     {
         //SlimeChildren（略:SC）
         if (collision.gameObject.tag == "SC")
         {
-            if (on == false)
+            if (a <= 99)
+            {
+                a++;
+            }
+            if (on == false&&a>=10)
             {
                 Door.GetComponent<Door1>().Right();
                 on = true;
@@ -19,12 +24,18 @@ public class Doorswitch : MonoBehaviour
             
         }
     }
+
     private void OnCollisionExit(Collision collision)
     {
+        if (a != 0)
+        {
+            a--;
+        }
+        
         //SlimeChildren（略:SC）
         if (collision.gameObject.tag == "SC")
         {
-            if (on == true)
+            if (on == true&&a<=9)
             {
                 Door.GetComponent<Door1>().Left();
                 on = false;
---
+++ Assets/MORI/scripts/SlimeShower.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeShower : MonoBehaviour
+{
+    public GameObject Slime;
+
+    public void slimeshower()
+    {
+
+    }
+}
