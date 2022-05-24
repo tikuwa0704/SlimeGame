46b087513340346a3506570087d792e7efe4d5fb
tikuwa0704 <103643302+QuickManEXE@users.noreply.github.com>
2022-05-24 11:30:43 +0900

    StageNameのUIを追加　GAMESTARの修正

---
+++ Assets/ANDO/Script/DOTween Scripts/DOFaseOutAnim.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class DOFaseOutAnim : MonoBehaviour
+{
+    [SerializeField]
+    private float beforefadeTime = 5.0f;
+
+    [SerializeField]
+    private float fadeTime = 1.0f;
+
+    [SerializeField]
+    bool isFade = false;
+
+    private Text text;
+
+    private Image image;
+
+    private CanvasGroup canvasGroup;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        beforefadeTime -= Time.deltaTime;
+
+        if(!isFade&&beforefadeTime <= 0)
+        {
+            isFade = true;
+
+            if(text)text.DOFade(0, fadeTime);
+            if(image)image.DOFade(0, fadeTime);
+        }
+
+    }
+}
