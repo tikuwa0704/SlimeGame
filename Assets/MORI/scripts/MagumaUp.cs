dac6bb3a6bd9b61518baa57a3961e62c390e19aa
しょるー <91645801+shorucuro-do@users.noreply.github.com>
2022-05-25 11:17:22 +0900

    修正

--- Assets/MORI/scripts/MagumaUp.cs
+++ Assets/MORI/scripts/MagumaUp.cs
@@ -14,4 +14,13 @@ public class MagumaUp : MonoBehaviour
             transform.Translate(0f, up, 0f);
         }
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Upswitch = false;
+            this.transform.Translate(new Vector3(-18.3f, -1.0f, 41.59f));
+        }
+    }
 }
